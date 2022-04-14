fd832f35db61e77b710c3bac4fe03ebceb5030e6
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-04-13 19:54:34 -0500

    Reading and Writing files

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // node.js is built around this concept of modules,
 const fs = require("fs");
 
-const hello = "Hello World";
-console.log(hello);
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+const textOut = `This is what we know about the avovado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written!");
