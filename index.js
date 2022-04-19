4a42d3c62e4abfd28aa51318f4834642a0125d5e
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-04-19 18:26:37 -0500

    HTML Templating: Filling the Templates

--- index.js
+++ index.js
@@ -33,7 +33,35 @@ const url = require("url");
 ///////////////////////////////
 //SERVER
 
+const replaceTemplate = (temp, product) => {
+  //this will make it so all the place holders will get replaced, and not just the first one regex /g flag
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
 //Top Level Code, it is blocking, but only runs once at the start
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
@@ -42,14 +70,28 @@ const server = http.createServer((req, res) => {
 
   const pathName = req.url;
 
+  // Overview Page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+    res.writeHead(200, { "Content-type": "text/html" });
+    //loop over dataObj, each iteration we replace the placeholder in the template card with the current product
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    // console.log(cardsHtml);
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    //Product Page
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+
+    //API
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     // console.log(productData);
     res.end(data);
+
+    //Not found
   } else {
     // a http header is a piece of info about the response we are sending back
     // the header and the status code always need to be set before we send out the response
